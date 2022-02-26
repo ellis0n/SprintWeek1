8b3cb671d432a957fef0d6c830b1f79adaeca654
markhannem <100211073+markhannem@users.noreply.github.com>
2022-02-26 17:17:20 -0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Written between February 25th 2022 and March 6th 2022
 # This program does, uh, what?
 
+import matplotlib.pyplot as plt
+
 # Function for calculating travel claims for NL Chocolate Factory
 def travelClaim():
     print("Travel Claim Program")
@@ -16,8 +18,139 @@ def strDate():
     print("Cool stuff")
 
 # Function that graphs total sales?
-def graphClaim():
-    print("Graph")
+
+
+def Graph():
+    # Function for Graph
+    Mths = ["Jan", "Feb", "Mar", "Apr'", "May", "June", "July", "Aug",
+            "Sept", "Oct", "Nov", "Dec"]
+
+    # Define Plot Space
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    # Formatting x/y-axis, titles and graph
+    xaxis = Mths
+    yaxis = Sales
+
+    ax.plot(xaxis, yaxis, color="cyan", marker="o")
+
+    plt.xlabel("Months", fontdict={'fontsize': 10, 'fontweight': 3, 'color': 'Crimson'})
+    plt.ylabel("Monthly Totals\n(dollars,$)", fontdict={'fontsize': 10, 'fontweight': 3, 'color': 'Crimson'})
+    plt.setp(ax.get_xticklabels(), rotation=45)
+
+    plt.title("NL Chocolate Company\nSales Totals Per Month", fontdict={'fontsize': 15,
+                                                                        'fontweight': 5, 'color': 'darkgoldenrod'})
+    plt.grid(True)
+
+    plt.show()
+
+    return plt.show()
+
+
+Sales = []
+while True:
+    print("Input Your Monthly Sales Total Below")
+    print()
+
+    while True:
+        try:
+            Jan = float(input("Enter the Total Sales For January: "))
+        except:
+            print("January must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Jan)
+            break
+    while True:
+        try:
+            Feb = float(input("Enter the Total Sales For February: "))
+        except:
+            print("February must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Feb)
+            break
+    while True:
+        try:
+            Mar = float(input("Enter the Total Sales For March: "))
+        except:
+            print("March must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Mar)
+            break
+    while True:
+        try:
+            Apr = float(input("Enter the Total Sales For April: "))
+        except:
+            print("April must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Apr)
+            break
+    while True:
+        try:
+            May = float(input("Enter the Total Sales For May: "))
+        except:
+            print("May must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(May)
+            break
+    while True:
+        try:
+            Jun = float(input("Enter the Total Sales For June: "))
+        except:
+            print("June must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Jun)
+            break
+    while True:
+        try:
+            Jul = float(input("Enter the Total Sales For July: "))
+        except:
+            print("July must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Jul)
+            break
+    while True:
+        try:
+            Aug = float(input("Enter the Total Sales For August: "))
+        except:
+            print("August must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Aug)
+            break
+    while True:
+        try:
+            Sep = float(input("Enter the Total Sales For September: "))
+        except:
+            print("September must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Sep)
+            break
+    while True:
+        try:
+            Oct = float(input("Enter the Total Sales For October: "))
+        except:
+            print("October cannot be blank.")
+        else:
+            Sales.append(Oct)
+            break
+    while True:
+        try:
+            Nov = float(input("Enter the Total Sales For November: "))
+        except:
+            print("November must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Nov)
+            break
+    while True:
+        try:
+            Dec = float(input("Enter the Total Sales For December: "))
+        except:
+            print("December must have a valid number entry please - re-enter.")
+        else:
+            Sales.append(Dec)
+            break
+    break
+
+Graph()
 
 
 #Main menu/program
